1abdc5570530c0294a90490cfcf1f10f8a0c9695
Timothy van den Beitel <2157002@talnet.nl>
2022-02-16 14:23:31 +0100

    2.2

---
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -0,0 +1,6 @@
+<?php
+$leeftijd= 0;
+for($leeftijd = 0 ; $leeftijd < 18; $leeftijd++){
+    echo"ik ben $leeftijd jaar, dus ik mag helaas nog niet stemmen. <br>";
+}
+echo "ik ben $leeftijd jaar oud dus ik heb stemrecht!";
