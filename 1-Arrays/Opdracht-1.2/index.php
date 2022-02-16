d90a76f86024f40e7fda4443ad94ba62e0be162e
Timothy van den Beitel <2157002@talnet.nl>
2022-02-16 13:15:46 +0100

    1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,9 @@
+<?php
+$persoon['voornaam']="Timothy";
+$persoon['achternaam']="van den Beitel";
+$persoon['leeftijd']=17;
+$persoon['opleiding']="Sofware Onwikkeling";
+
+echo $persoon['voornaam'] . " " . $persoon['achternaam'] . " is" . $persoon['leeftijd'] . " jaar oud en is student aan het ROC van Amsterdam aan de opleiding" . $persoon['opleiding'];
+
+
