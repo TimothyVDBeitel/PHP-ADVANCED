92f7f12c992d740af1174c2f444bb327c14e6d6a
Timothy van den Beitel <2157002@talnet.nl>
2022-02-16 13:57:56 +0100

    2.1 klaar

---
+++ 2-Loops/Opdracht-2.1/loops-while.php
@@ -0,0 +1,10 @@
+<?php
+$leeftijd =0;
+
+while($leeftijd < 18) {
+    echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+    $leeftijd++;
+
+}
+
+echo "ik ben $leeftijd jaar oud, en heb stemrecht" ;
