f239f6d15ef285af44ec2ab260e066f112cba74b
Timothy van den Beitel <2157002@talnet.nl>
2022-02-18 11:54:28 +0100

    2.4

--- 2-Loops/Opdracht-2.4/loops-foreach-deel2.php
+++ 2-Loops/Opdracht-2.4/loops-foreach-deel2.php
@@ -3,9 +3,13 @@
 <?php
 
 $klassen = array("8A", "8B", "8C", "8D", "9A", "9B", "9C", "9D", "9E");
+echo "<ul>";
 foreach ($klassen as $klas) {
-    echo "<option value=$klas>$klas</option>";
+
+    echo "<li><option value=$klas>$klas</option></li>";
 }
+echo "</ul>";
+
 ?>
 </select>
 <input type="submit" value="Submit">
