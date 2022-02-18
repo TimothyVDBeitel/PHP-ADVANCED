5d5f8f4d2fa55d47e8e8b28847e8a511b1a6073b
Timothy van den Beitel <2157002@talnet.nl>
2022-02-18 11:57:12 +0100

    2.3

--- 2-Loops/Opdracht-2.3/loops-foreach-deel1.php
+++ 2-Loops/Opdracht-2.3/loops-foreach-deel1.php
@@ -1,7 +1,8 @@
 <?php
 $dagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
-
+echo "<ul>";
 foreach ($dagen as $value){
 
     echo "<li>$value </li>"."<br>"; 
 }
+echo "</ul>";
