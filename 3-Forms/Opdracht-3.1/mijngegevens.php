1bd7a4f6298917ebe7d03cd594d545e83e52ef35
Timothy van den Beitel <2157002@talnet.nl>
2022-03-01 12:57:27 +0100

    3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,6 @@
+<?php
+echo "voornaam: ".$_GET['name'] . "<br>";
+echo "klas: ".$_GET['klas'] . "<br>";
+echo "leeftijd: ".$_GET['leeftijd'] . "<br>";
+echo "straat: ".$_GET['straat'] . "<br>";
+echo "plaats: ".$_GET['plaats'] . "<br>";
