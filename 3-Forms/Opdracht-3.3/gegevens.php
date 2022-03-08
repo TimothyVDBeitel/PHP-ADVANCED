eb7b3afdd48b744507cee35878142a57dd92cc89
Timothy van den Beitel <2157002@talnet.nl>
2022-03-08 09:07:56 +0100

    3.3

--- 3-Forms/Opdracht-3.3/gegevens.php
+++ 3-Forms/Opdracht-3.3/gegevens.php
@@ -7,13 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
 <?php
-
-echo "artiest: ".$_GET['artist'] . "<br>";
+    echo "artiest: ".$_GET['artist'] . "<br>";
 echo "Titel: ".$_GET['liedje'] . "<br>";
 echo "jaar van uitkomst: ".$_GET['uitgave'] . "<br>";
 echo "muzieksoort: ".$_GET['muzieksoort'] . "<br>";
-http://localhost/PHP-ADVANCED/3-Forms/Opdracht-3.3/gegevens.php?artist=RHCP&liedje=Can%27t+stop&uitgave=2003&muzieksoort=Rock
+http://localhost/PHP-ADVANCED/3-Forms/Opdracht-3.3/gegevens.php?artist=RHCP&liedje=Can%27t+stop&uitgave=2003&muzieksoort=Rock ?>
+
+</body>
+</html>
